5c61aae082af658e8a7a82ebf5bcb6808f264f72
pmatamala <paula.matama@inputsoft.cl>
2026-04-04 22:01:15 -0300

    creacion del footer

--- templates/JavaScript/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const toggleBtn = document.querySelector(".menu-toggle");
-  const navMenu = document.querySelector(".nav-menu");
-
-  toggleBtn.addEventListener("click", () => {
-    navMenu.classList.toggle("active");
-  });
-});
